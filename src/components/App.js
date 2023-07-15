13c58563abb9fec6c6cefdf5582c54e6949c1905
anitha-anii <anithachowdary355@gmail.com>
2023-07-15 16:57:15 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+
+// const App = () => {
+//   const [isVisible, setIsVisible] = useState('');
+
+//   const handleButtonClick = () => {
+//     setIsVisible("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+//   };
+
+//   return ( 
+//     <div id="main">
+      
+//         <p id="para" className="show">{isVisible}</p>
+     
+//       <button id="click" onClick={handleButtonClick}>
+//         Click me!
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default App
+
 import React, { useState } from "react";
 import './../styles/App.css';
 
-
 const App = () => {
-  const [isVisible, setIsVisible] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleButtonClick = () => {
-    setIsVisible("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+    setIsVisible(true);
   };
 
-  return (
+  return ( 
     <div id="main">
-      
-        <p id="para" className="show">{isVisible}</p>
-     
+      <p id="para" className={isVisible ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
       <button id="click" onClick={handleButtonClick}>
         Click me!
       </button>
@@ -21,5 +45,4 @@ const App = () => {
   );
 };
 
-
-export default App
+export default App;
